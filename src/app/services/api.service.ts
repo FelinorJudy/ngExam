bea83e8c8ce454b0b52856e709fe957303cf670c
FelinorJudy <giorgiodp1996@gmail.com>
2025-04-01 18:52:15 +0200

    test(servizio Api testato)

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,17 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class apiService {
+    private headers = new HttpHeaders ({
+            'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmU5OGViNTFkMjBjYmY1OTJjNjFiYzA5MTk1OTkzYyIsIm5iZiI6MTc0MzQxMzMwOS42NDYwMDAxLCJzdWIiOiI2N2VhNjAzZDcwMGE2YTk0YzZlNTgyM2EiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.P_uU3osJHeQKpMvKANuaE3ZJ38HFjw99YmG0FiS9uZE'
+        })
     private apiUrlGenres = 'https://api.themoviedb.org/3/genre/movie/list?language=en'
     constructor(private http: HttpClient) {}
+    
     getGenres(): Observable<any> {
-        return this.http.get<any>(this.apiUrlGenres)
+        return this.http.get<any>(this.apiUrlGenres, {headers: this.headers})
     }
 }
 
