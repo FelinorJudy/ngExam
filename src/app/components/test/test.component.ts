e8e84368ef32442676cf87abf510b60c46942062
annavispo <annavispo.dev@gmail.com>
2025-04-02 09:26:13 +0200

    feat(test.component.css): add css menu

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { apiService } from '../../services/api.service';
 import { error } from 'console';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './test.component.html',
-  styleUrl: './test.component.css'
+  styleUrl: './test.component.css',
 })
-export class TestComponent implements OnInit{
-  genres: any[] = []
+export class TestComponent implements OnInit {
+  genres: any[] = [];
+
   constructor(private apiService: apiService) {}
 
   ngOnInit(): void {
-      this.apiService.getGenres().subscribe(
-        data => {
-          this.genres = data.genres
-        },
-        error => console.error("fanculo", error)
-      )
+    this.apiService.getGenres().subscribe({
+      next: (data) => {
+        this.genres = data.genres;
+      },
+      error: (err) => console.error('Errore nel recupero dei generi', error),
+    });
+  }
+
+
+  onGenreClick(genreId: number) {
+    console.log('Genere selezionato:', genreId);
   }
 }
