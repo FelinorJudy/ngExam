9f6703524479159e358e62cfda4e384b93faa500
FelinorJudy <giorgiodp1996@gmail.com>
2025-04-03 09:15:09 +0200

    fix(fixedScroll): adesso è possibile scrollare all'infinito all'interno della pagina dei film. #DA MIGLIORARE: possibilità di aggiungere un @defer per migliorare le performances

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,6 +8,7 @@ export class apiService {
             'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmU5OGViNTFkMjBjYmY1OTJjNjFiYzA5MTk1OTkzYyIsIm5iZiI6MTc0MzQxMzMwOS42NDYwMDAxLCJzdWIiOiI2N2VhNjAzZDcwMGE2YTk0YzZlNTgyM2EiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.P_uU3osJHeQKpMvKANuaE3ZJ38HFjw99YmG0FiS9uZE'
         })
     private apiUrl = 'https://api.themoviedb.org/3'
+    private apiUrlTrending = 'https://api.themoviedb.org/3/trending/movie/week?language=en-US'
 
     constructor(private http: HttpClient) {}
 
