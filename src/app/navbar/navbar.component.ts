7dc33e3647ab26baaa6f7a3b7846584b26758970
annavispo <annavispo.dev@gmail.com>
2025-04-05 17:40:28 +0200

    risolto problema bg,creato component navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { apiService } from '../services/api.service';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-navbar',
+  imports: [FormsModule,RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  search: string = '';
+  searchResults: any[] = [];
+
+  constructor(private apiService: apiService) {}
+
+  onSearch() {
+    console.log('Ricerca:', this.search);
+    this.apiService.getMovieByTitle(this.search).subscribe({
+      next: (data) => {
+        this.searchResults = data.results;
+      },
+      error: (err) => console.log('Errore nella ricerca del film'),
+    });
+  }
+}
