73232be7cdf36c17621a8916f16181c620c6dd5d
annavispo <annavispo.dev@gmail.com>
2025-04-07 12:14:52 +0200

    creazione api per movie-details /dettagli film

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -5,26 +5,38 @@ import { StorageService } from '../../services/storage.service';
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.css'
+  styleUrl: './movie-details.component.css',
 })
 export class MovieDetailsComponent implements OnInit {
   @Input() movieId!: string;
   visto: boolean = false;
-
+  overview: string = '';
   tagline: string = '';
   photoUrl: string = '';
+  title: string = '';
+  release_date: string= '';
+  runtime: number=0;
+  vote_average: number=0;
 
-  constructor(private apiService: apiService, private storageService: StorageService) {}
+  constructor(
+    private apiService: apiService,
+    private storageService: StorageService
+  ) {}
 
   ngOnInit(): void {
     // Controlla se il film è già stato visto
     this.visto = this.storageService.getFilmVisti()[this.movieId] || false;
 
     // Ottieni le informazioni sul film
-    this.apiService.getInfoFromMovie(this.movieId).subscribe(data => {
+    this.apiService.getInfoFromMovie(this.movieId).subscribe((data) => {
       this.tagline = data.tagline;
       this.photoUrl = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
-    });
+      this.overview = data.overview;
+      this.title=data.title;
+      this.release_date=data.release_data;
+      this.runtime=data.runtime;
+      this.vote_average=data.vote_average;
+   });
   }
 
   // Funzione per segnare il film come "già visto"
